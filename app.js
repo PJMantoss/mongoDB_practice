d0c12d46f603c93f52dbcad2c7db61891b0d6e92
PJMantoss <joelptoss@gmail.com>
2021-10-07 13:41:43 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const mongoPractice = require('./mongo');
 
 
 const app = express();
 
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
+
+app.use(express.json());
 
 app.post('/products');
 
