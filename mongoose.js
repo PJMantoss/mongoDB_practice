27dcdc4f654100d2a984cbec959614afeb357a84
PJMantoss <joelptoss@gmail.com>
2021-10-07 18:22:14 +0100

    update controller file - mongoose

--- mongoose.js
+++ mongoose.js
@@ -2,4 +2,9 @@ const mongoose = require('mongoose');
 
 const Product = require('./models/product');
 
-const createProduct = async (req, res, next) => {};
+const createProduct = async (req, res, next) => {
+    const createdProduct = new Product({
+        name: req.body.name,
+        price: req.body.price
+    })
+};
