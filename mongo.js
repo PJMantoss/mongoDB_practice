19304f0963bcafe9827cc2ff62ce779d780ab8e5
PJMantoss <joelptoss@gmail.com>
2021-10-07 16:00:25 +0100

    update mongo.js file

--- mongo.js
+++ mongo.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 
-const url = 'mongodb+srv://Toss:helloma@cluster0.kxgzl.mongodb.net/products_test?retryWrites=true&w=majority';
+const url = 'mongodb+srv://pj:kingdom@cluster0.kxgzl.mongodb.net/products_test?retryWrites=true&w=majority';
 
 const createProduct = async (req, res, next) => {
     const newProduct = {
